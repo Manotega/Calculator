001e44a3288d41f353e042dd9604c7aaea53f14e
Lucas <lucas_manteiga@outlook.com>
2025-03-12 23:53:20 -0300

    agora vai

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let secondNumber = null;
 let operator = null;
 let secondOperator = null;
 let resultado = null;
-let conta = [];
+let isResultGiven = false;
+let isOperatorActive = false;
+let isSequenceCalc = false;
 
 const tela = document.querySelector(".tela");
 const botoesNumero = document.querySelectorAll(".botao-numero");
@@ -13,56 +15,60 @@ const botaoResultado = document.querySelector(".botao-resultado");
 
 botoesNumero.forEach(button => {
     button.addEventListener("click", () => {
+        if (tela.textContent === '0') {
+            tela.innerHTML = "";
+        }
+        if (isOperatorActive) {
+            tela.innerHTML = "";
+            isOperatorActive = false;
+        }
         tela.textContent = `${tela.textContent}${button.textContent}`;
-        if (conta.includes("calculado")) {
+    
+        if (operator != null && isResultGiven) {
+            isResultGiven = false;
+        }
+
+        if (isResultGiven && !isSequenceCalc) {
             tela.textContent = button.textContent;
             limpar();
         }
-        if (conta.includes("continuado")) {
+    
+        if (isSequenceCalc) {
             tela.textContent = button.textContent;
-            conta.splice(0);
-            conta.push(firstNumber);
+            isSequenceCalc = false;
         }
-
-        if (conta.includes(firstNumber)) {
-            secondNumber = +tela.textContent;
-            console.log({secondNumber});
-        } else {
+    
+        if (operator === null) {
             firstNumber = +tela.textContent;
-            console.log({firstNumber});
+        } else {
+            secondNumber = +tela.textContent;
         }
     })
 })
 
 botoesOperacao.forEach(button => {
     button.addEventListener("click", () => {
-        if (operator === null) {
-            operator = button.textContent;
-        } else {
-            secondOperator = button.textContent;
-        }
-        
-        tela.innerHTML = '';
-        
-        if (conta.length >= 3) {
-            conta.splice(0);
-            conta.push(firstNumber);
+        if (isSequenceCalc) {
+            tela.innerHTML = "";
         }
 
-        if (conta.length === 1) {
+        if (isResultGiven) {
+            isSequenceCalc = true;
+        }
+
+        if (operator === null) {
+            operator = button.textContent;
+            isOperatorActive = true;
+        } else {
+            secondOperator = button.textContent;
             resultado = operate(firstNumber, operator, secondNumber);
             tela.textContent = resultado;
             firstNumber = resultado;
-            conta.push("continuado");
-            conta.push(resultado);
+            operator = secondOperator;
+            secondOperator = null;
+            isSequenceCalc = true;
+            isOperatorActive = true;
         }
-
-        if (!conta.includes(firstNumber)) {
-            conta.push(firstNumber);
-        }
-
-        console.log({operator});
-        console.log({secondOperator});
     })
 })
 
@@ -72,11 +78,15 @@ botaoLimpar.addEventListener("click", () => {
 })
 
 botaoResultado.addEventListener("click", () => {
-    let resultado = operate(firstNumber, operator, secondNumber);
-    tela.textContent = resultado;
-    firstNumber = resultado;
-    conta.push("calculado");
-    conta.push(resultado);
+    if (firstNumber === null || operator === null) {
+        return null
+    } else {
+        let resultado = operate(firstNumber, operator, secondNumber);
+        tela.textContent = resultado;
+        firstNumber = resultado;
+        isResultGiven = true;
+        operator = null;
+    }
 })
 
 function limpar() {
@@ -84,40 +94,38 @@ function limpar() {
     operator = null;
     secondOperator = null;
     secondNumber = null;
-    conta = [];
     resultado = null;
+    isSequenceCalc = false;
+    isResultGiven = false;
+    isOperatorActive = false;
+    tela.textContent = '0';
 }
 
 function add(a, b) {
     return a + b;
 }
-
 function subtract(a, b) {
     return a - b;
 }
-
 function multiply(a, b) {
     return a * b;
 }
-
 function divide(a, b) {
+    if (b === 0) {
+        return "calma calabreso"
+    }
     return a / b;
 }
-
 function operate(number1, op, number2) {
     switch (op) {
         case '+':
             return add(number1, number2);
-        
         case '-':
-            return subtract(number1, number2);
-        
+            return subtract(number1, number2);       
         case 'X':
             return multiply(number1, number2);
-
         case '/':
-            return divide(number1, number2)
-        
+            return divide(number1, number2)        
         default:
             return "ERROR!";
     }
